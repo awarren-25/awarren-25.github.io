db3c63b9db13da94bbab3935904adb897d014f07
awarren-25 <theoc.benjir@gmail.com>
2025-12-01 15:36:05 +0000

    completed circularity project, thanks to looking at Skylar's work and from his feedback

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -34,17 +34,13 @@ var circles = [];
 
 
         // TODO 3 : Call the drawCircle() function
-drawCircle();
-drawCircle();
-drawCircle();
-drawCircle();
-drawCircle();
 
 
         // TODO 7 : Use a loop to create multiple circles
-
-
-
+var i = drawCircle();
+for (var i = 0; i < 100; i++) {
+    drawCircle();
+}
 
         ///////////////////
         // PROGRAM LOGIC //
@@ -57,22 +53,15 @@ drawCircle();
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
-physikz.updatePosition(circles[0]);
-physikz.updatePosition(circles[1]);
-physikz.updatePosition(circles[2]);
-physikz.updatePosition(circles[3]);
-physikz.updatePosition(circles[4]);
+
             
             // TODO 5 : Call game.checkCirclePosition() on your circles
-game.checkCirclePosition(circles[0]);
-game.checkCirclePosition(circles[1]);
-game.checkCirclePosition(circles[2]);
-game.checkCirclePosition(circles[3]);
-game.checkCirclePosition(circles[4]);
 
             // TODO 8 / TODO 9 : Iterate over the array
-           
-            
+      for (var i = 0; i < circles.length; i++) {
+        physikz.updatePosition(circles[i]);
+        game.checkCirclePosition(circles[i]);
+      }
         }
     
         /* 
